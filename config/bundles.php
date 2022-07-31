1ecd6d3e16a4c775069b3f2a9c8a11f12ced7c2b
Viacheslav <vm.php.dev@gmail.com>
2022-07-31 17:10:56 +0300

    add nelmio doc bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
